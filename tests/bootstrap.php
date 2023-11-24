c842ef284479dea05f1b04e5eec628a9eefeb604
Alexander Piskun <bigcat88@icloud.com>
2023-11-24 10:52:49 +0300

    fixed type

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../../../tests/bootstrap.php';
 
-use OCA\Jira\AppInfo\Application;
+use OCA\Notion\AppInfo\Application;
 
 // remain compatible with stable26
 \OC_App::loadApp(Application::APP_ID);
